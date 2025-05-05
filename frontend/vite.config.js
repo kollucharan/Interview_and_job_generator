bed458101ebaff9bea1675be458bb34a5c70ac8e
kollucharan <132676020+kollucharan@users.noreply.github.com>
2025-05-05 11:41:38 +0530

    added redirect

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // base: '/jdgenerator/',
   plugins: [react()],
+  base: '/jdgenerator/', 
   // base: '/jdgenerator/',
   // base: '/jdgenerator/',
   // base: '/',
