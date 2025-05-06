98222c3f67d9b18873928b1b368f54658df876c0
kollucharan <132676020+kollucharan@users.noreply.github.com>
2025-05-06 18:09:02 +0530

    modified config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // base: '/jdgenerator/',
   plugins: [react()],
-  base: '/jdgenerator/', 
+  base: '/jd-generator/', 
   // base: '/jdgenerator/',
   // base: '/jdgenerator/',
   // base: '/',
